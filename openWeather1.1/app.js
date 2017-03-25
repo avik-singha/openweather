098f4cbaba5e16835ac6741b8986081e632d670c
Avik Singha <aviksingha8@gmail.com>
2017-03-25 17:19:04 +0530

    Place Autocomplete Implementation

--- openWeather1.1/app.js
+++ openWeather1.1/app.js
@@ -3,7 +3,7 @@ var app=angular.module("temperatureApp",[]);
 	app.controller("temperatureController",	function($scope,$http){
 		var absoluteTemp=273.16;
 		$scope.getTemperature=function(cityName){			
-			$http.get("http://api.openweathermap.org/data/2.5/weather?q="+cityName+",IN&APPID=feae8ac11fdaa955a8ae907051606abe")
+			$http.get("http://api.openweathermap.org/data/2.5/weather?q="+cityName+",&APPID=012581a1cd5d98c0b591970c753d2a12")
 				.then(function(response){
 					$scope.lat=response.data.coord.lat;
 					$scope.long=response.data.coord.lon;
@@ -19,6 +19,7 @@ var app=angular.module("temperatureApp",[]);
 					$scope.wind_speed=response.data.wind.speed;
 					$scope.weather_description=response.data.weather["0"].description;
 					$scope.weather_icon=response.data.weather.icon;
+					$scope.city_name=cityName;
 					// $scope.=response.data.;
 				},function(response){
 					$scope.temperatures="Some Error Occured";
